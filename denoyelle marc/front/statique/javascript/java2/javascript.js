80c546a91426f22e48bb0a62764aa3eafe1fdcef
denoyelle marc <denoyellemarc@orange.frr>
2022-02-23 14:40:10 +0100

    nbvgbhgfc

--- denoyelle marc/front/statique/javascript/java2/javascript.js
+++ denoyelle marc/front/statique/javascript/java2/javascript.js
@@ -1,10 +1,4 @@
-var bouton1= document.getElementById("Id_btn1");
-bouton1.addEventListener("click",clickbtn1);
 
-function clickbtn1(){
-
-    alert("Vous avez cliqué sur :\n Alert()");
-}
 
 var bouton2= document.getElementById("Id_btn2");
 bouton2.addEventListener("click",clickbtn2);
@@ -22,37 +16,28 @@ function clickbtn2(){
     alert("Vous avez cliqué sur Annuler");
     return;
     }  
-    
-
-    alert("bonjour :\n\n "+nom+"\n " +prenom);
-}
-
-var bouton3= document.getElementById("Id_btn3");
-bouton3.addEventListener("click",clickbtn3);
-
-function clickbtn3(){
-
-    var reponse = confirm("Veuillez cliquer sur OK ou bien Annuler");
+       
+    var reponse = confirm("Etes vous un homme cliquer sur OK ou bien Annuler");
     if (reponse == true) {
-        alert("Vous avez cliqué sur OK");
+        alert("bonjour monsieur :\n\n "+nom+"\n " +prenom);
+        
     } 
     else {
         alert("Vous avez cliqué sur Annuler");
     }
 
+
+
+
+
+
+    
+
+    
 }
 
-var bouton4= document.getElementById("Id_btn4");
-bouton4.addEventListener("click",clickbtn4);
 
-function clickbtn4(){
 
-    var prenom = prompt("Veuillez saisir votre prénom");
-    if(prenom==null){
-        alert("Vous avez cliqué sur Annuler");
-    }
-    else{
-        console.log("Voici le prénom que vous avez saisi: "+prenom);
-        alert("Vérifiez en Console (F12), ce que vous venez de saisir...");
-    }
-}
+
+    
+    
